4463cccb7ab90f63f6efef844b7500e2e60fc97c
Ravi Shankar Singh <civilstudy2024@gmail.com>
2025-01-03 00:27:54 +0530

    Routing

---
+++ Modules-1/File System/Roting/SimpleRouting.js
@@ -0,0 +1,25 @@
+const http = require('http');
+// const { server } = require('typescript');
+const url = require('url');
+
+ const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/Home') {
+        res.end('This is the home page');
+    } else if (pathName === '/product') {
+        res.end('This is the product page');
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+            'my-own-header' : 'Hello, World!'
+        });
+        res.end('<h1>This page is not Found!</h1>');
+    }
+});
+    
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('server is running at http://127.0.0.1:8000')
+});
+
