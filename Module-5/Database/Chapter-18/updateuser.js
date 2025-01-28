f1ecd038680b5d7f0d267a1d2cc88cff81aaaaa1
Ravi Shankar Singh <civilstudy2024@gmail.com>
2025-01-28 23:42:46 +0530

    Error Set

--- Module-5/Database/Chapter-18/updateuser.js
+++ Module-5/Database/Chapter-18/updateuser.js
@@ -2,9 +2,12 @@ const dbConnect = require('./mongodb')
 
  async function Update() {
     const db = await dbConnect();
-    const result = await db.updataOne({name:"Ravi"},{emailid: "bholasingh@gmail.com"})
+    const result = await db.updataOne([
+        {name:"Ravi"},{emailid: "bholasingh@gmail.com"},
+        {name:"Sukanyaa Singh"},{name:"Sukanyaa"},
+       
+        
 
-    
-    
-    
+    ])
 }
+Update()
