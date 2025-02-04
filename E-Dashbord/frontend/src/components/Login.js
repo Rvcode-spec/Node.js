3a58ce3fdbb5b1acf39f0467df949c36f94ce84d
Ravi Shankar Singh <civilstudy2024@gmail.com>
2025-02-04 23:52:28 +0530

    Login Page

--- E-Dashbord/frontend/src/components/Login.js
+++ E-Dashbord/frontend/src/components/Login.js
@@ -4,7 +4,8 @@ export default function Login() {
     const [email,setEmail] = useState();
     const [password,setPassword]= useState();
 
-    const handleLogin=()=>{
+    const handleLogin= async ()=>{
+        let result=await fetch('')
         console.log(email,password);
         
     }
